b4ec12567117c0dfd6006c3cc9f1d9d8d09c80ef
misha378888888888888 <mishakmedved@gmail.com>
2025-01-16 21:23:29 +0700

    map

--- main.py
+++ main.py
@@ -1,50 +1,41 @@
 import folium
 
-map = folium.Map(
-    location=[53.722279, 91.443704],
-    zoom_start = 16,
-    tiles=None,
-    attribution_control=0
-)
-
-# слои карты
-folium.TileLayer('CyclOSM', name="CyclOSM").add_to(map)
-folium.TileLayer('Esri.WorldImagery', name="Esri World Imagery").add_to(map)
-folium.TileLayer('OpenStreetMap', name="OpenStreetMap").add_to(map)
-# улицы я не понимать как это делать
-#folium.PolyLine('lenina_coordinates', name="lenina_coordinates").add_to(map)
-#folium.PolyLine('shetinkina_coordinates', name="shetinkina_coordinates").add_to(map)
-#folium.PolyLine('krulova_coordinates', name="krulova_coordinates").add_to(map)
-#folium.PolyLine('chertigasheva_coordinates', name="chertigasheva_coordinates").add_to(map)
-
-# координаты улиц
-lenina_coordinates = [
-    [53.7211, 91.4333],
-    [53.722755,  91.4495],
-    [53.723056, 91.4539],
-    [53.7234, 91.4565],
-]
-shetinkina_coordinates = [
-    [53.726186,91.44252],
-    [53.734935,91.43993],
-    [53.733805,91.440309],
-    [53.727508,91.442126],
-    [53.7248349,91.442895],
-    [53.724819,91.442882],
-    [53.722302, 91.44362],
-    [53.717319,91.445112],
-]
-
-krulova_coordinates = [
-    [53.736146, 91.452544],
-    [53.735022, 91.439977],
-    [53.735036, 91.440496],
-    [53.735308, 91.444087],
-    [53.735334, 91.444388],
-    [53.735387, 91.444843],
-]
-
-chertigasheva_coordinates = [
+# Данные для улиц
+streets = {
+    "Щетинкина": {
+        "markers": [
+            {"name": " 1", "coords": [53.7252, 91.4428]},
+            {"name": " 2", "coords": [53.7199, 91.4444]}
+        ],
+        "line": [
+            [53.726186, 91.44252],
+            [53.734935, 91.43993],
+            [53.733805, 91.440309],
+            [53.727508, 91.442126],
+            [53.7248349, 91.442895],
+            [53.724819, 91.442882],
+            [53.722302, 91.44362],
+            [53.717319, 91.445112],
+        ]
+    },
+    "Ленина": {
+        "markers": [
+            {"name": " 3", "coords": [53.7215, 91.4381]},
+            {"name": " 4", "coords": [53.7224, 91.4468]}
+        ],
+        "line": [
+            [53.7211, 91.4333],
+            [53.722755, 91.4495],
+            [53.723056, 91.4539],
+            [53.7234, 91.4565],
+        ]
+    },
+    "Чертыгашева": {
+        "markers": [
+            {"name": "5", "coords": [53.724500, 91.440000]},
+            {"name": "6", "coords": [53.7246, 91.4414]}
+        ],
+        "line": [
     [53.723808, 91.432518],
     [53.724108, 91.435558],
     [53.724206, 91.436697],
@@ -63,62 +54,58 @@ chertigasheva_coordinates = [
     [53.725840, 91.453251],
     [53.725965, 91.454592],
     [53.726027, 91.455169],
-]
 
-# линии улиц
-html_lenina = '''<h1>Проспект Ленина</h1>
-              <img src="https://www.on-walking.com/files/abakan/011.jpg" width="300px" height="200px">  '''
-html_shetinkina = '''<h1>Улица Щетинкина</h1>
-              <img src="https://i3.photo.2gis.com/main/branch/69/70000001040677149/common" width="300px" height="200px">  '''
-folium.PolyLine(
-    locations=lenina_coordinates,
-    color="#ff5200",
-    weight=5,
-    tooltip="Проспект Ленина",
-    popup=html_lenina,
-).add_to(map)
-folium.PolyLine(
-    locations=shetinkina_coordinates,
-    color="#ff5200",
-    weight=5,
-    tooltip="Улица Щетинкина",
-    popup=html_shetinkina,
-).add_to(map)
-folium.PolyLine(
-    locations=krulova_coordinates,
-    color="#ff5200",
-    weight=5,
-    tooltip="Улица Крылова",
-    popup="Улица Крылова",
-).add_to(map)
-folium.PolyLine(
-    locations=chertigasheva_coordinates,
-    color="#ff5200",
-    weight=5,
-    tooltip="Улица Чертыгашева",
-    popup="Улица Чертыгашева",
-).add_to(map)
+        ]
+    },
+    "Крылова": {
+        "markers": [
+            {"name": "Маркер 1", "coords": [53.7352,  91.4429]},
+            {"name": "Маркер 2", "coords": [53.7360, 91.4519]}
+        ],
+        "line": [
+            [53.7350, 91.4400],
+            [53.7352, 91.4429],
+            [53.7353, 91.4441],
+            [53.7360, 91.4519],
+        ]
+    }
+}
 
-# Тестовые маркеры для карты
-folium.Marker(
-    location=[53.721565, 91.436445],
-    tooltip="Иконка 1",
-    popup="Тест1",
-    icon=folium.Icon(icon="seedling", prefix="fa", color="red"),
-).add_to(map)
-folium.Marker(
-    location=[53.721978, 91.440966],
-    tooltip="Иконка 2",
-    popup="Тест2",
-    icon=folium.Icon(icon="seedling", prefix="fa", color="green"),
-).add_to(map)
-folium.Marker(
-    location=[53.722445, 91.445457],
-    tooltip="Иконка 3",
-    popup="Тест3",
-    icon=folium.Icon(icon="seedling", prefix="fa", color="orange"), # orange
-).add_to(map)
+m = folium.Map(
+    location=[53.722279, 91.443704],
+    zoom_start = 16,
+    tiles=None,
+    attribution_control=0
+)
+# слои карты
+folium.TileLayer('CyclOSM', name="CyclOSM").add_to(m)
+folium.TileLayer('Esri.WorldImagery', name="Esri World Imagery").add_to(m)
+folium.TileLayer('OpenStreetMap', name="OpenStreetMap").add_to(m)
 
-folium.LayerControl().add_to(map)
+for street_name, data in streets.items():
+    # Создаем группу для каждой улицы
+    street_group = folium.FeatureGroup(name=street_name)
 
-map.save("map.html")
+    # Добавляем маркеры
+    for marker in data["markers"]:
+        folium.Marker(
+            location=marker["coords"],
+            popup=f"<b>{marker['name']}</b>",
+            icon=folium.Icon(icon="seedling", prefix="fa", color="green"),
+        ).add_to(street_group)
+
+    # Добавляем линию
+    folium.PolyLine(
+        locations=data["line"],
+        color="#ff5200",  # Цвет линии
+        weight=10,      # Толщина линии
+    ).add_to(street_group)
+
+    # Добавляем группу улицы на карту
+    street_group.add_to(m)
+
+# Добавляем контроллер слоев для переключения между улицами collapsed=False
+folium.LayerControl().add_to(m)
+
+# Сохраняем карту
+m.save("map.html")
